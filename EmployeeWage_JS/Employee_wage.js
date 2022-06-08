ccebb2c8370e6c270d5fde978df613d2bcd0e839
bhokaresneha <bhokaresneha20@gmail.com>
2022-06-08 09:53:01 +0530

    Added : Cheking Employee is Present Or Absent

--- EmployeeWage_JS/Employee_wage.js
+++ EmployeeWage_JS/Employee_wage.js
@@ -1,1 +1,16 @@
+/*---------------------------------Employee Wage--------------------------------------*/
+
 console.log("Welcome to Employee Wage Problem");
+
+/*Check Employe is Present or Absent Using Random Function*/
+
+let Attendance=Math.floor(Math.random()*2);
+console.log(Attendance);
+
+//Checking employee is present or absent
+//Present=1    Absent=0
+if(Attendance==1){
+    console.log("Employee is Present !!!");
+}else{
+    console.log("Employee is Absent !!!");
+}
