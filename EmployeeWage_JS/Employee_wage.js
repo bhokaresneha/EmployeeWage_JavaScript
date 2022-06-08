b1f4656fc08052751a2d42bd953aea6eb1f82b9c
bhokaresneha <bhokaresneha20@gmail.com>
2022-06-08 10:20:38 +0530

    Added :Using Function Calculating Daily Wage

--- EmployeeWage_JS/Employee_wage.js
+++ EmployeeWage_JS/Employee_wage.js
@@ -1,1 +1,45 @@
+/*---------------------------------Employee Wage--------------------------------------*/
+
 console.log("Welcome to Employee Wage Problem");
+
+//variable initialization
+
+const PART_TIME_HRs=4;
+const FULL_TIME_HRs=8;
+const WAGE_PER_HRs=20;
+let Attendance=Math.floor(Math.random()*2);
+console.log(Attendance);
+
+//Checking employee is present or absent
+//Present=1    Absent=0
+if(Attendance==1){
+    console.log("Employee is Present !!!");
+
+    let Employee_Hours=getWorkignHours();
+
+    console.log("Employee is Present for "+Employee_Hours+" Hours");
+    let Employee_Wage=Employee_Hours*WAGE_PER_HRs;
+    console.log("Employee Wage is : "+Employee_Wage);    
+
+   }else{
+    console.log("Employee is Absent");
+}
+
+function getWorkignHours(){
+    let Attendance_Time=Math.floor(Math.random()*2);
+
+    //Part_Time=0 Full_Time=1
+    switch(Attendance_Time){
+        case 0:
+            return PART_TIME_HRs;
+            break;
+
+        case 1:
+            return FULL_TIME_HRs   
+            break;
+        default:
+            console.log(" ");
+            break;
+        }
+ 
+}
